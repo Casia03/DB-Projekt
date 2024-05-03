071310911c11e55131090a479c434d56594a073c
Casia <119851945+Casia03@users.noreply.github.com>
2024-05-03 17:23:46 +0200

    bla bla

--- DatenbankenProjekt/src/app/app-routing.module.ts
+++ DatenbankenProjekt/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FilmComponent } from './film/film.component';
+import { FilmListComponent } from './film-list/film-list.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: '/film', component: FilmComponent },
+  { path: '/film-list', component: FilmListComponent },
+  { path: '/login', component: LoginComponent },
+  { path: '/register', component: RegisterComponent },
+  { path: '/user', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
