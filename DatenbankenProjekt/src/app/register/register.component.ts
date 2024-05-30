2dd0a3708a51afe4df732e63a63498eeeccaff3f
SmugCoder <jancenkevin@gmail.com>
2024-05-30 17:22:01 +0200

    Fixes
    Habe Fehler gefixt die beim vorherigen commit entstanden sind

--- DatenbankenProjekt/src/app/register/register.component.ts
+++ DatenbankenProjekt/src/app/register/register.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Form Submitted', this.registerForm.value);
+    } else {
+      console.log('Form not valid');
+    }
+  }
 }
