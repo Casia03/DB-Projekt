f3446d935047f9e3df63ec8874930558d9efecee
SmugCoder <jancenkevin@gmail.com>
2024-05-30 22:02:16 +0200

    Homepage
    habe mal ein paar änderungen an der homepage gemacht, wenn es einer schafft einen pfeil als butten in der sidebar zu machen wäre das gut

--- DatenbankenProjekt/src/app/app-routing.module.ts
+++ DatenbankenProjekt/src/app/app-routing.module.ts
@@ -7,14 +7,16 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserComponent } from './user/user.component';
 
-
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: '/film', component: FilmComponent },
-  { path: '/film-list', component: FilmListComponent },
-  { path: '/login', component: LoginComponent },
-  { path: '/register', component: RegisterComponent },
-  { path: '/user', component: UserComponent },
+  { path: '', redirectTo: 'home-page', pathMatch: 'full' }, // Standardroute zur Homepage
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'film', component: FilmComponent },
+  { path: 'film-list', component: FilmListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'user', component: UserComponent },
+  // Optionale ERROR 404-Seite
+  { path: '**', redirectTo: 'home-page' } // Wildcard-Route, die auf die Homepage umleitet
 ];
 
 @NgModule({
--- DatenbankenProjekt/src/app/home-page/home-page.component.ts
+++ DatenbankenProjekt/src/app/home-page/home-page.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  isSidebarOpen = false; // Initialer Zustand der Sidebar
 
+  toggleSidebar() { // Methode zum Umschalten des Zustands der Sidebar
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  closeSidebar() { // Methode zum Schließen der Sidebar
+    this.isSidebarOpen = false;
+  }
 }
