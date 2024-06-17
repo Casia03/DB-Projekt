15bd56cb105ce1dfcc47269aac7d61ea058d9827
Casia <119851945+Casia03@users.noreply.github.com>
2024-06-17 11:35:45 +0200

    upgraded login user component

--- DatenbankenProjekt/src/app/user-login/user-login.component.spec.ts
+++ DatenbankenProjekt/src/app/user-login/user-login.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms'; // Import FormsModule to use [(ngModel)]
 import { UserLoginComponent } from './user-login.component';
 
 describe('UserLoginComponent', () => {
@@ -8,7 +8,8 @@ describe('UserLoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserLoginComponent]
+      declarations: [UserLoginComponent],
+      imports: [FormsModule] // Include FormsModule in the testing module
     })
     .compileComponents();
     
--- DatenbankenProjekt/src/app/user-login/user-login.component.ts
+++ DatenbankenProjekt/src/app/user-login/user-login.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
-
 export class UserLoginComponent {
 
   signupUsers: any[] = [];
@@ -21,23 +19,51 @@ export class UserLoginComponent {
     password: ''
   };
 
+  // Control which form to show
+  showLoginForm: boolean = true;
+
   constructor() {
+    // Load users from localStorage
+    const storedUsers = localStorage.getItem('signUpUsers');
+    this.signupUsers = storedUsers ? JSON.parse(storedUsers) : [];
   }
 
-  onSignUp(){
-    console.log("User " + this.signupObj + " Succesfully signed up")
-    this.signupUsers.push(this.signupObj)
-    localStorage.setItem('signUpUsers', JSON.stringify(this.signupUsers))
-    this.signupObj = {
+  toggleForm() {
+    this.showLoginForm = !this.showLoginForm;
+  }
+
+  onSignUp() {
+    const userExists = this.signupUsers.some(user => user.username === this.signupObj.username);
+    
+    if (!userExists) {
+      this.signupUsers.push({ ...this.signupObj }); // Create a new object to avoid reference issues
+      localStorage.setItem('signUpUsers', JSON.stringify(this.signupUsers));
+      console.log("User " + JSON.stringify(this.signupObj) + " successfully signed up");
+
+      // Clear the form
+      this.signupObj = {
+        username: '',
+        email: '',
+        password: ''
+      };
+    } else {
+      console.log("User already exists");
+    }
+  }
+
+  onLogin() {
+    const user = this.signupUsers.find(user => user.username === this.loginObj.username && user.password === this.loginObj.password);
+
+    if (user) {
+      console.log("Login successful for user " + this.loginObj.username);
+    } else {
+      console.log("Invalid username or password");
+    }
+
+    // Clear the login form
+    this.loginObj = {
       username: '',
-      email: '',
       password: ''
     };
   }
-
-
-  onLogin(){
-
-  }
-
 }
