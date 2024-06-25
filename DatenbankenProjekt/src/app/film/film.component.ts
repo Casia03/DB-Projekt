964f0e0861051cfb0504944c749fdab43f925544
SmugCoder <jancenkevin@gmail.com>
2024-06-25 17:47:41 +0200

    Changes Film

--- DatenbankenProjekt/src/app/film/film.component.ts
+++ DatenbankenProjekt/src/app/film/film.component.ts
@@ -1,17 +1,38 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-film',
   templateUrl: './film.component.html',
   styleUrls: ['./film.component.css']
 })
-export class FilmComponent {
+export class FilmComponent implements OnInit {
   isSidebarOpen = false; // Initialer Zustand der Sidebar
+  films: any[] = []; //Array der Filme beinhaltet
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getFilms(); //Filmdaten werden aufgerufen wenn das component initialisiert wird
+  }
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
 
+  getFilms() {
+    this.http.get<any[]>('/film').subscribe(
+      (data: any[]) => {
+        console.log(data);
+        this.films = data;
+      },
+      (error: HttpErrorResponse) => {
+        console.error('Error fetching films', error);
+      }
+    );
+  }
+
+
+  /*
   http:any;
   constructor(client:HttpClient) {
     this.http = client;
@@ -23,4 +44,6 @@ export class FilmComponent {
       this.film = data;
     });
   }
+  */
+
 }
