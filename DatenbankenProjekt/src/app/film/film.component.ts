5fbc9c6b65f7818415aaab74b1ca5fbd3f457acf
SmugCoder <jancenkevin@gmail.com>
2024-06-25 16:03:34 +0200

    Changes

--- DatenbankenProjekt/src/app/film/film.component.ts
+++ DatenbankenProjekt/src/app/film/film.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-film',
   templateUrl: './film.component.html',
-  styleUrl: './film.component.css'
+  styleUrls: ['./film.component.css']
 })
 export class FilmComponent {
   isSidebarOpen = false; // Initialer Zustand der Sidebar
