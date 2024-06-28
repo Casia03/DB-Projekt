fae26388b1289d7b37e1bd296b78017c8db452d1
mrmm1231 <maik130503@gmail.com>
2024-06-28 18:22:57 +0200

    daten der filme

--- DatenbankenProjekt/server.js
+++ DatenbankenProjekt/server.js
@@ -9,7 +9,7 @@ const con = mysql.createConnection({
     database: "sakila",
     host: "localhost",
     user: "root",
-    password: "aamijnawssh123"
+    password: "1231"
 });
 
 // support parsing of application/json type post data
